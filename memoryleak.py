aef20a868f79748b51fa27437203cd0dd7060d0a
LuckyHacker <onni.hakkari@gmail.com>
2018-08-13 14:26:12 +0300

    Add memory leak protection script

---
+++ memoryleak.py
@@ -0,0 +1,20 @@
+import psutil
+import threading
+import time
+
+import __main__
+
+
+def monitor_memory():
+    limit = 90
+    while 1:
+        percent = psutil.virtual_memory()[2]
+        if percent > limit:
+            print("Memory usage over {}.".format(limit))
+            p = psutil.Process(__main__.os.getpid())
+            p.terminate()
+
+        time.sleep(1)
+
+
+threading.Thread(target=monitor_memory).start()
